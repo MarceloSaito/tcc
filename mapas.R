fc348d5a92b40231a1180189d9e3d7f723bc9d9d
MarceloSaito <mn_saito@hotmail.com>
2023-03-02 17:12:29 -0300

    retorno tcc

--- mapas.R
+++ mapas.R
@@ -1,7 +1,7 @@
 ###########---------------------bibs--------------#######
 
 library(foreign)
-library(sf)
+#library(sf)    <-- ta dando erro sla pq
 library(tidyr)
 library(geobr)
 library(ggplot2)
@@ -11,7 +11,7 @@ library(viridisLite)
 library(viridis)
 library(Hmisc)
 
-setwd("C:/Users/mn_sa/Documents/R/Version control/tcc")
+setwd("C:/Users/mn_sa/Documents/tcc")
 
 ###########---------------------grau de participacao sp-------------------####
 
@@ -42,12 +42,12 @@ setwd("C:/Users/mn_sa/Documents/R/Version control/tcc")
 ###########---------------------grau part brasil----------#####
 
 
-muni_br <- read_municipality(code_muni = "all")
+muni_br <- read_municipality(code_muni = "all", year = 2020)
 
-dados_grau_part <- read.csv("C:/Users/mn_sa/Documents/R/Version control/tcc/grau_part.csv")
+dados_grau_part <- read.csv("C:/Users/mn_sa/Documents/tcc/grau_part.csv")
 
-dados_grau_part_br <- dados_grau_part %>% 
-        select(codigo, grau_part) %>% 
+dados_grau_part_br <- dados_grau_part |> 
+        select(codigo, grau_part) |> 
         rename(code_muni=codigo) 
 
 
@@ -67,11 +67,11 @@ ggplot(grau_part_geo_br) +
 ###########---------------------pagamento da divida br metodo original -----------------####
 
 # 
-cap_pag_div <- read.csv("C:/Users/mn_sa/Documents/R/Version control/tcc/cap_pag_div.csv")
+cap_pag_div <- read.csv("C:/Users/mn_sa/Documents/tcc/cap_pag_div.csv")
 
 cap_pag_div1 <-cap_pag_div %>%
-                mutate(nova_escala = (2 * cap_pag_div + 115.54) / 152.6) %>%
-                rename(code_muni=codigo) %>%
+                mutate(nova_escala = (2 * cap_pag_div + 115.54) / 152.6) |>
+                rename(code_muni=codigo) |>
                 select(code_muni, nova_escala)
 
 
@@ -89,10 +89,10 @@ ggplot(cap_pag_div_br_geo) +
 ###########---------------------pagamento da divida lrf com dados sem e quad 1 a 2-------------------####
 
 
-divida_lrf_dados <- read.csv("C:/Users/mn_sa/Documents/R/Version control/tcc/divida_lrf.csv")
+divida_lrf_dados <- read.csv("C:/Users/mn_sa/Documents/tcc/divida_lrf.csv")
 
-divida_lrf_dados <- divida_lrf_dados %>% 
-        mutate(indice_div = ((2 * (dcl / rcl) + 6.14) / 7.57)) %>% 
+divida_lrf_dados <- divida_lrf_dados |>
+        mutate(indice_div = ((2 * (dcl / rcl) + 6.14) / 7.57)) |>
         select(indice_div, code_muni)
 
 divida_lrf_dados_geo <- left_join(muni_br, divida_lrf_dados,  by = "code_muni")
@@ -137,43 +137,43 @@ muni_br <- read_municipality(code_muni = "all")
 
 quad <- read.csv("quad.csv", sep =";", skip = 5, header = T, dec = ",")
 
-rcl_quad <- quad %>% 
+rcl_quad <- quad |> 
   filter(Conta == "RECEITA CORRENTE LÍQUIDA - RCL",
          Coluna == "Até o 3º Quadrimestre",
-         PODER == "Executivo") %>% 
-  rename(code_muni = Cod.IBGE) %>% 
+         PODER == "Executivo") |> 
+  rename(code_muni = Cod.IBGE) |> 
   select(code_muni, Valor)
 
-dcl_quad <- quad %>% 
+dcl_quad <- quad |>
   filter(Conta == "DÍVIDA CONSOLIDADA LÍQUIDA (DCL) (III) = (I - II)",
          Coluna == "Até o 3º Quadrimestre",
-         PODER == "Executivo") %>% 
-  rename(code_muni = Cod.IBGE) %>% 
+         PODER == "Executivo") |>
+  rename(code_muni = Cod.IBGE) |> 
   select(code_muni, Valor)
   
 
 sem <- read.csv("sem.csv", sep =";", skip = 5, header = T, dec = ",")
 
-rcl_sem<- sem %>% 
+rcl_sem<- sem |> 
   filter(Conta == "RECEITA CORRENTE LÍQUIDA - RCL",
          Coluna == "Até o 2º Semestre",
-         PODER == "Executivo") %>% 
-  rename(code_muni = Cod.IBGE) %>% 
+         PODER == "Executivo") |> 
+  rename(code_muni = Cod.IBGE) |> 
   select(code_muni, Valor)
 
-dcl_sem <- sem %>% 
+dcl_sem <- sem |> 
   filter(Conta == "DÍVIDA CONSOLIDADA LÍQUIDA (DCL) (III) = (I - II)",
          Coluna == "Até o 2º Semestre",
-         PODER == "Executivo") %>% 
-  rename(code_muni = Cod.IBGE) %>% 
+         PODER == "Executivo") |> 
+  rename(code_muni = Cod.IBGE) |> 
   select(code_muni, Valor)
 
 # unindo rcl e dcl 
 
-rcl_full <- bind_rows(rcl_quad, rcl_sem) %>% 
+rcl_full <- bind_rows(rcl_quad, rcl_sem) |> 
   rename(rcl = Valor)
 
-dcl_full <- bind_rows(dcl_quad, dcl_sem) %>% 
+dcl_full <- bind_rows(dcl_quad, dcl_sem) |> 
   rename(dcl = Valor)
 
 muni_rcl <- left_join(muni_br, rcl_full, by = "code_muni")
@@ -194,15 +194,15 @@ muni_rcl <- left_join(muni_br, rcl_full, by = "code_muni")
   
 #essa versao considera apenas numeros entre 0 e 1.2
 
-muni_rcl_dcl_geo <- left_join(muni_rcl, dcl_full, by = "code_muni") %>% 
-  drop_na() %>% ####essa é a linha de código que retira informações de alguns municipios
+muni_rcl_dcl_geo <- left_join(muni_rcl, dcl_full, by = "code_muni") |> 
+  drop_na() |> ####essa é a linha de código que retira informações de alguns municipios
   mutate(dcl_rcl = dcl / rcl, 
          dcl_rcl_02 = case_when(
           dcl_rcl > 1.2 ~ 2,
           dcl_rcl < 0 ~ 0,
           TRUE ~ as.numeric(as.character(dcl_rcl))),
          indice_02 = (((2 * dcl_rcl_02) - (2 * min(dcl_rcl_02))) / (max(dcl_rcl_02) - min(dcl_rcl_02)) - 2) * (-1)
-         )%>%
+         )|>
   select(name_muni,code_muni, abbrev_state ,code_state, dcl_rcl, dcl_rcl_02, indice_02, geom)
   
 
@@ -240,53 +240,53 @@ ggplot(muni_rcl_dcl_geo) +
 
 muni_br<- read_municipality(code_muni = "all")
 
-cons_inter <- read.csv("C:/Users/mn_sa/Documents/R/Version control/tcc/cons_inter.csv")
+cons_inter <- read.csv("C:/Users/mn_sa/Documents/tcc/cons_inter.csv")
 
-cons_inter <- cons_inter %>% 
-        select(code_muni, eq1) %>% 
-        rename(eq1_cons_inter = eq1)
+cons_inter <- cons_inter |> 
+        select(code_muni, eq1) |> 
+         rename(eq1_cons_inter = eq1) #talvez essas linhas estejam erradas
 
 cons_inter_geo <- left_join(muni_br,cons_inter, by = "code_muni")
 
 
-ggplot(cons_inter_geo)+
+cons_inter_plot <- ggplot(cons_inter_geo)+
         geom_sf(aes(fill = eq1, col = eq1, geometry = geom)) +
         scale_color_gradientn(colours = brewer.pal(9, 'RdYlGn'))+
         scale_fill_gradientn(colours = brewer.pal(9, 'RdYlGn' ))
 
 
-###########---------------------prpcapita sem limite---------------------#################
+###########---------------------prpcapita sem limite (nao estou utilizando este---------------------#################
 
-prpcapita <- read.csv("C:/Users/mn_sa/Documents/R/Version control/tcc/prcapita.csv")
+#prpcapita <- read.csv("C:/Users/mn_sa/Documents/tcc/prcapita.csv")
 
-muni_br<- read_municipality(code_muni = "all")
+#muni_br<- read_municipality(code_muni = "all")
 
-prpcapita <- prpcapita %>% 
-  drop_na() %>% 
-  mutate(eq1_prpcapita2 = (((prpc - min(prpc)) / (max(prpc) - min(prpc))) * 5) + 1) %>% 
-  rename(code_muni = codigo)
+#prpcapita <- prpcapita |> 
+#  drop_na() |> 
+#  mutate(eq1_prpcapita2 = (((prpc - min(prpc)) / (max(prpc) - min(prpc))) * 5) + 1) # |>
+ # # rename(code_muni = codigo) # ta dando pau aqui
 
 # prpcapita <- prpcapita %>% 
 #   rename(code_muni = codigo) %>% 
 #   select(code_muni, eq1_prpcapita)
 # 
 
-prpcapita_geo <- left_join(muni_br, prpcapita, by = "code_muni")
+#prpcapita_geo <- left_join(muni_br, prpcapita, by = "code_muni")
 
-ggplot(prpcapita_geo)+
-  geom_sf(aes(fill = eq1_prpcapita2, col = eq1_prpcapita2, geometry = geom)) +
-  scale_color_gradientn(colours = brewer.pal(9, 'RdYlGn'))+
-  scale_fill_gradientn(colours = brewer.pal(9, 'RdYlGn' ))
+#ggplot(prpcapita_geo)+
+#  geom_sf(aes(fill = eq1_prpcapita2, col = eq1_prpcapita2, geometry = geom)) +
+#  scale_color_gradientn(colours = brewer.pal(9, 'RdYlGn'))+
+#  scale_fill_gradientn(colours = brewer.pal(9, 'RdYlGn' ))
 
 
 ###########---------------------prpcapita limite 3k---------------------#################
 
 muni_br<- read_municipality(code_muni = "all")
 
-prpcapita <- read.csv("C:/Users/mn_sa/Documents/R/Version control/tcc/prcapita.csv")
+prpcapita <- read.csv("C:/Users/mn_sa/Documents/tcc/prcapita.csv")
 
-prpcapita <- prpcapita %>% 
-  drop_na() %>% 
+prpcapita <- prpcapita |> 
+  drop_na() |> 
   mutate(eq1_prpcapita3k = (((prpcapita_max3k - min(prpcapita_max3k )) / (max(prpcapita_max3k ) - min(prpcapita_max3k ))) * 5) + 1) %>% 
   rename(code_muni = codigo) %>% 
   select(code_muni, eq1_prpcapita3k)
@@ -312,8 +312,8 @@ cap_fin <- full_join(muni_rcl_dcl_geo, prpcapita, by = "code_muni")
 
 cap_fin_full <- full_join(cap_fin, cons_inter, by = "code_muni")
   
-capacidade_financeira_dados <- cap_fin_full %>% 
-  mutate(capacidade_financeira = (eq1_prpcapita3k * 1/3) + (indice_02) + (eq1_cons_inter * 1/3)) %>% 
+capacidade_financeira_dados <- cap_fin_full |> 
+  mutate(capacidade_financeira = (eq1_prpcapita3k * 1/3) + (indice_02) + (eq1_cons_inter * 1/3)) |> 
   select(name_muni, code_muni, code_state, abbrev_state,
          capacidade_financeira, geom)
 
@@ -326,9 +326,9 @@ ggplot(capacidade_financeira_dados)+
 ###########---------------------capacidade gerencial---------------###############
 muni_br<- read_municipality(code_muni = "all")
 
-cap_ger <- read.csv("C:/Users/mn_sa/Documents/R/Version control/tcc/cap_ger.csv")
+cap_ger <- read.csv("C:/Users/mn_sa/Documents/tcc/cap_ger.csv")
 
-cap_ger <- cap_ger %>% 
+cap_ger <- cap_ger |> 
   mutate(iptu = if_else(iptu==1, 6, 1),
          ano_da_lei = case_when(ano_da_lei < 1970 ~ 6,
                                 ano_da_lei %in% 1970:1990 ~ 5,
@@ -343,7 +343,7 @@ cap_ger <- cap_ger %>%
          cg = (iptu_ano_lei * 1/4) + (eq1_instru_gestao * 1/4) +(eq1_iptu_cad_imo * 1/4) + (eq1_instru_plan * 1/4)
          ) 
 
-ind_final <- cap_ger %>% 
+ind_final <- cap_ger |> 
   select(code_muni, cg)
 
 ind_final_geo <- left_join(muni_br, ind_final, by = "code_muni")  
@@ -357,7 +357,7 @@ ggplot(ind_final_geo)+
 
 iqim1 <- left_join(capacidade_financeira_dados, ind_final, by = "code_muni")
 
-iqim <- left_join(iqim1, dados_grau_part_br, by = "code_muni") %>% 
+iqim <- left_join(iqim1, dados_grau_part_br, by = "code_muni") |> 
   mutate(iqim_final = (capacidade_financeira * 1/3) + (cg * 1/3) + (grau_part * 1/3))
 
 ggplot(iqim) +
